0bfaa026a4e7782bb4ff8e94a52af6d33bdf62fa
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-03-11 18:38:14 +0530

    Skills Section Done.

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
@@ -20,9 +20,8 @@ const Work = () => {
       setWorks(data);
       setFilterWork(data);
     })
-  }, [])
+  }, []);
   
-
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }])
@@ -59,7 +58,7 @@ const Work = () => {
     transition={{ duration: 0.5, delayChildren: 0.5 }}
     className="app__work-portfolio"
     >
-      {filterWork.map((work, index) => (
+      {filterWork?.map((work, index) => (
         <div className="app__work-item app__flex" key={index}>
           <div className="app__work-img app__flex">
             <img src={urlFor(work.imgUrl)} alt={work.name} />
@@ -108,4 +107,8 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work');
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  'app__primarybg'
+);
