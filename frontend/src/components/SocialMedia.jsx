f07b6036cf866261e80c83807fb7c941f45ef7e4
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-03-13 13:56:04 +0530

    Footer/Contact Section & Other Stuff Done.

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,21 +1,48 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-        <div>
-            <BsTwitter />
-        </div>
-        <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
+      <div>
+        <a
+          href="https://twitter.com/ShaikhKhizr"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.facebook.com/khizr.shaikh.14/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.instagram.com/shaikh_khizr/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/khizr-shaikh-42a6b01b1/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SocialMedia;
